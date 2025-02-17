87577950627ddef5d162825060a453a9fdcf3cfc
alexcolemandata <coleman.g.alex@gmail.com>
2025-02-17 18:37:51 +1100

    log attempts + errors, retry if invalid name

--- src/scratch_pydantic_ai/scratch.py
+++ src/scratch_pydantic_ai/scratch.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 
+from string import ascii_letters
 from pydantic_ai.exceptions import UnexpectedModelBehavior
 from pydantic_ai.models.openai import OpenAIModel
-from pydantic import Field, ValidationError
+from pydantic import Field, ValidationError, AfterValidator
 from pydantic import BaseModel
 from rich.console import Console
 from rich.live import Live
@@ -15,7 +16,7 @@ from pydantic_ai import Agent
 HOST = "http://localhost:11434/v1/"
 
 MODEL_NAME = "llama3.1:8b"
-NUM_CHARACTERS = 5
+NUM_CHARACTERS = 10
 MAX_ATTEMPTS = 5
 
 client = OpenAI(api_key="ollama", base_url=HOST)
@@ -25,6 +26,13 @@ ollama_model = OpenAIModel(
     base_url=HOST,
     api_key="ollama_api_key",
 )
+
+
+def is_song_starting_with_letter(song: str) -> str:
+    if song[0] not in ascii_letters:
+        raise ValueError(f"{song} does not start with a letter!")
+
+    return song
 
 
 class Character(BaseModel):
@@ -39,6 +47,7 @@ class Character(BaseModel):
     song: Annotated[
         str,
         Field(description="Favourite 80's song, in the form {title} - {artist}"),
+        AfterValidator(is_song_starting_with_letter),
     ]
 
 
@@ -57,53 +66,65 @@ name_flagger = Agent(
 )
 
 
+def init_table() -> Table:
+    table = Table(
+        title="Historical Characters",
+        caption=f"Streaming Structured responses from {MODEL_NAME}",
+        width=120,
+    )
+    table.add_column("ID", justify="right")
+    table.add_column("Name")
+    table.add_column("Quote")
+    table.add_column("Occupation", justify="right")
+    table.add_column("Song", justify="right")
+    table.add_column("Attempts", justify="center")
+
+    return table
+
+
 def main():
-    console = Console()
-    with Live("\n" * 36, console=console) as live:
+    characters: list[Character] = []
+    table = init_table()
+
+    with Live(table, refresh_per_second=4) as live:
+        console = live.console
         console.print("Requesting data...", style="cyan")
-
-        characters: list[Character] = []
-
-        table = Table(
-            title="Historical Characters",
-            caption=f"Streaming Structured responses from {MODEL_NAME}",
-            width=120,
-        )
-        table.add_column("ID", justify="right")
-        table.add_column("Name")
-        table.add_column("Quote")
-        table.add_column("Occupation", justify="right")
-        table.add_column("Song", justify="right")
-        table.add_column("Valid Name?", justify="right")
-        table.add_column("Attempts", justify="center")
 
         while len(characters) < NUM_CHARACTERS:
             names = [character.name for character in characters]
             songs = [character.song for character in characters]
             attempts = 0
 
-            msg = "Generate me details of a significant historical figure"
+            msg = "Generate details of a historical figure"
             if names:
-                msg += ". Do not pick: " + ",".join(f"'{name}'" for name in names)
+                msg += ". Do not pick: " + ",".join(names)
             if songs:
-                msg += " Do not use these songs: " + ",".join(
-                    f"'{song}'" for song in songs
+                msg += ". Do not use these songs: " + ", ".join(
+                    song.split("-")[0].strip() for song in songs
                 )
+
+            console.print(f"Generating Character {len(characters) + 1}")
+            console.print(f"[italic]{msg}")
 
             while attempts < MAX_ATTEMPTS:
                 try:
                     result = character_agent.run_sync(msg).data
                 except UnexpectedModelBehavior as e:
                     attempts += 1
+                    console.print(f"Attempt {attempts}: [red]{e}")
                 else:
-                    break
+                    try:
+                        is_valid_name = name_flagger.run_sync(result.name).data
+                    except UnexpectedModelBehavior as e:
+                        is_valid_name = str(e)
+                    else:
+                        if not is_valid_name:
+                            attempts += 1
+                            console.print(f"Invalid name generated: [red]{result.name}")
+                        else:
+                            break
             else:
                 continue
-
-            try:
-                is_valid_name = name_flagger.run_sync(result.name).data
-            except UnexpectedModelBehavior as e:
-                is_valid_name = str(e)
 
             character = result
             characters.append(character)
@@ -114,7 +135,6 @@ def main():
                 character.quote or "…",
                 character.occupation or "…",
                 character.song or "…",
-                str(is_valid_name),
                 str(attempts),
             )
             live.update(table)
